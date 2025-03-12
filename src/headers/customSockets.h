c6eca5be9c1e13dad2fcaae014f01a8c75bf8818
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 12:55:55 +0100

    Ajout de l'inclusion des en-têtes spécifiques aux sockets pour la compatibilité avec Linux et Windows dans le fichier d'en-tête des sockets personnalisés

--- src/headers/customSockets.h
+++ src/headers/customSockets.h
@@ -5,9 +5,15 @@
 #define CUSTOM_SOCKETS_HEADER
 
 #ifdef __linux__
-
-#else defined(_WIN32) || defined(_WIN64)
-
+	#include <netinet/in.h>
+	#include <sys/socket.h>
+	#include <sys/types.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+#else defined(_WIN32)
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+	#include <windows.h>
 #endif
 
 #endif
