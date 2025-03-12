c4bbbfa0990d047a46b1662242d5509047fdb823
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 13:25:31 +0100

    Suppression de la fonction de création de socket Windows dans customSocket.c

--- src/definitions/customSocket.c
+++ src/definitions/customSocket.c
@@ -1,16 +1,1 @@
-#include "../headers/customSockets.h"
 
-#ifdef _WIN32
-
-void createWinSocket(osSpecific_t* osSpecific) {
-	if (WSAStartup(MAKEWORD(2, 2), &osSpecific->wsaData) != 0) {
-		fprintf(stderr, "WSAStartup failed with error: %d\n", WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-}
-
-#else
-
-void createUnixSocket();
-
-#endif
