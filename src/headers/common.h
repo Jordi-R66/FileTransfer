dea74e9249049373a0168037d7ee2dbc80677691
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 12:52:37 +0100

    Ajout de l'inclusion de <string.h> dans le fichier d'en-tête commun

--- src/headers/common.h
+++ src/headers/common.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
 /* bool, true and false are keywords.  */
