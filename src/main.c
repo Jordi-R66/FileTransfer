c2b06491e5c4eeb5681f9a39526b38303d7eacf6
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 21:14:08 +0100

    Ajout de la gestion de l'envoi de fichiers dans la fonction sender

--- src/main.c
+++ src/main.c
@@ -7,19 +7,47 @@ void sender(uint8_t remote[4], uint8_t local[4], uint16_t port, string* filename
 	socketParams_t socketParams = generateParams(local, remote, DEFAULT_PORT, Sender);
 	socketParams_t remoteParams;
 
+	socketParams.isMain = true;
+
 	createSocket(&socketParams);
 
 	bindSocket(&socketParams);
 
 	listenToConnections(&socketParams, &remoteParams);
 
-	ssize_t receivedFromRemote = recv(remoteParams.socket_fd, buffer, BUFFER_SIZE, 0);
+	ssize_t receivedFromRemote = recv(remoteParams.fd, buffer, BUFFER_SIZE, 0);
 
 	if (receivedFromRemote < 0) {
 		fprintf(stderr, "Failed to receive initiation byte from remote host\n");
 		exit(EXIT_FAILURE);
 	}
 
+	for (size_t i=0; i<(size_t)receivedFromRemote; i++) {
+		buffer[i] = 0;
+	}
+
+	FILE* fp = fopen(*filename, "r");
+	size_t filesize = getFileSize(*filename);
+
+	size_t readBytes = 1;
+
+	while (readBytes > 0) {
+		readBytes = fread(buffer, 1, BUFFER_SIZE, fp);
+
+		send(remoteParams.fd, buffer, readBytes, 0);
+
+		for (size_t i=0; i<readBytes; i++) {
+			buffer[i] = 0;
+		}
+	}
+
+	const char EOT = 0x4;
+
+	send(remoteParams.fd, &EOT, 1, 0);
+
+	fclose(fp);
+
+	closeSocket(&remoteParams);
 	closeSocket(&socketParams);
 }
 
