98f5556779c2cbf98ea9b335d15e89fbb10e4107
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 20:59:48 +0100

    Ajout de la gestion des connexions entrantes et réception des données dans la fonction sender

--- src/main.c
+++ src/main.c
@@ -5,10 +5,20 @@ char buffer[BUFFER_SIZE];
 
 void sender(uint8_t remote[4], uint8_t local[4], uint16_t port, string* filename) {
 	socketParams_t socketParams = generateParams(local, remote, DEFAULT_PORT, Sender);
+	socketParams_t remoteParams;
 
 	createSocket(&socketParams);
 
 	bindSocket(&socketParams);
+
+	listenToConnections(&socketParams, &remoteParams);
+
+	ssize_t receivedFromRemote = recv(remoteParams.socket_fd, buffer, BUFFER_SIZE, 0);
+
+	if (receivedFromRemote < 0) {
+		fprintf(stderr, "Failed to receive initiation byte from remote host\n");
+		exit(EXIT_FAILURE);
+	}
 
 	closeSocket(&socketParams);
 }
