90e04268598def9bd22f1db15c72492bf6eac8e0
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 14:14:29 +0100

    Ajout de la structure socketParams et de l'énumération ConnType dans common.h, suppression de customTypes.h

--- src/headers/common.h
+++ src/headers/common.h
@@ -2,16 +2,38 @@
 
 #ifndef COMMON_HEADER
 
-#define COMMON_HEADER
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
+	#define COMMON_HEADER
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <stdint.h>
+	#include <string.h>
 
-#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
-/* bool, true and false are keywords.  */
-#else
-#include <stdbool.h>
-#endif
+	#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
+		/* bool, true and false are keywords.  */
+	#else
+		#include <stdbool.h>
+	#endif
+
+	enum ConnType {
+		Sender = 0,
+		Receiver = 1
+	};
+
+	typedef enum ConnType ConnType_t;
+
+	struct socketParams {
+		int socket_fd;
+
+		uint32_t srcAddr;
+		uint32_t destAddr;
+
+		uint16_t port;
+
+		ConnType_t connType;
+	};
+
+	typedef struct socketParams socketParams_t;
+
+	typedef struct sockaddr_in sockaddr_in_t;
 
 #endif
