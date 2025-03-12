4cd5429b9458eb990014289f53695f05106f7825
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 20:54:14 +0100

    Initialisation du tampon dans la fonction sender pour éviter les valeurs indéfinies

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include "headers/common.h"
 #include "headers/customSockets.h"
 
+char buffer[BUFFER_SIZE];
+
 void sender(uint8_t remote[4], uint8_t local[4], uint16_t port, string* filename) {
 	socketParams_t socketParams = generateParams(local, remote, DEFAULT_PORT, Sender);
 
 	createSocket(&socketParams);
 
 	bindSocket(&socketParams);
+
+	for (size_t i=0; i<BUFFER_SIZE; i++) {
+		buffer[i] = 0;
+	}
 
 	closeSocket(&socketParams);
 }
@@ -25,7 +31,7 @@ int main() {
 	uint8_t local[4];
 	uint8_t remote[4];
 
-	
+
 
 	return 0;
 }
