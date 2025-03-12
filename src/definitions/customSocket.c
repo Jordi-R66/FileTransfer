f2d43ca58ab5e1254d746756f715ed45c1a793ee
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-12 19:45:49 +0100

    Ajout de la fonction generateParams pour générer une structure de paramètres de socket

--- src/definitions/customSocket.c
+++ src/definitions/customSocket.c
@@ -18,4 +18,18 @@ void closeSocket(socketParams_t* socketParams) {
 	#endif
 }
 
+socketParams_t generateParams(uint8_t localIp[4], uint8_t remoteIp[4], uint16_t port, ConnType_t ConnType) {
+	socketParams_t output;
+
+	output.connType = ConnType;
+	output.localIp = *(uint32_t*)localIp;
+	output.remoteIp = *(uint32_t*)remoteIp;
+
+	output.socketAddress.sin_addr.s_addr = output.localIp;
+	output.socketAddress.sin_family = DEFAULT_DOMAIN;
+	output.socketAddress.sin_port = DEFAULT_PORT;
+
+	return output;
+}
+
 #endif
--- src/headers/customSockets.h
+++ src/headers/customSockets.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #ifndef CUSTOM_SOCKETS_HEADER
-
 	#ifdef __linux__
 		#include "linuxSocket.h"
 	#elif defined(_WIN32)
@@ -32,6 +31,17 @@
 		void bindSocket(socketParams_t* socketParams);
 
 		void closeSocket(socketParams_t* socketParams);
+
+		/**
+		 * @brief Generates a parameter struct
+		 * 
+		 * @param localIp uint8_t array (length 4
+		 * @param remoteIp uint8_t array (length 4
+		 * @param port uint16_t port
+		 * @param ConnType Connection type, either Sender or Receiver
+		 * @return socketParams_t 
+		 */
+		socketParams_t generateParams(uint8_t localIp[4], uint8_t remoteIp[4], uint16_t port, ConnType_t ConnType);
 
 	#endif
 #endif
