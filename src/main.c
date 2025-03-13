fb202d5ab4c3a56662c1684fdde94f8cf2e9ae97
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-13 13:04:33 +0100

    Réorganisation des inclusions et nettoyage du code dans main.c

--- src/main.c
+++ src/main.c
@@ -1,25 +1,13 @@
 #include "headers/common.h"
-#include "headers/customSockets.h"
-
-char buffer[BUFFER_SIZE]; // Buffer for data
-Endianness_t sysEndianness; // System endianness
-uint16_t commInitVal = 0xABCD; // Arbitrary value to check endianness and initConn
-
-
-
-
-
+#include "headers/receiver.h"
+#include "headers/sender.h"
+#include "headers/endianness.h"
 
 int main() {
-	sysEndianness = getEndian();
 	uint8_t local[4];
 	uint8_t remote[4];
 
 	string filename = "test.wav";
-
-	for (size_t i = 0; i < BUFFER_SIZE; i++) {
-		buffer[i] = 0;
-	}
 
 	sender(remote, local, DEFAULT_PORT, &filename);
 
