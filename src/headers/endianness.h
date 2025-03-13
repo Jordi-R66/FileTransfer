11ad03cf70a47366007f3b75db9e08dc08b2fe9d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-13 08:46:47 +0100

    Ajout de la documentation pour les fonctions getEndian et swapEndianness

--- src/headers/endianness.h
+++ src/headers/endianness.h
@@ -9,7 +9,19 @@ enum Endianness {
 
 typedef enum Endianness Endianness_t;
 
+/**
+ * @brief Get the Endian object (either Big or Little)
+ * 
+ * @return Endianness_t 
+ */
 Endianness_t getEndian();
+
+/**
+ * @brief Swaps a data's endianness
+ * 
+ * @param data The pointer to the data
+ * @param size The size of the data (bytes)
+ */
 void swapEndianness(void* data, size_t size);
 
 #define ENDIANNESS_HEADER
