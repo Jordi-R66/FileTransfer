9e2b595745de97e22945fa73f4954fcf381ed97d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-13 08:55:19 +0100

    Correction de la configuration du port dans generateParams et ajout de l'indicateur isMain dans la fonction receiver

--- src/definitions/customSocket.c
+++ src/definitions/customSocket.c
@@ -60,7 +60,7 @@ socketParams_t generateParams(uint8_t localIp[4], uint8_t remoteIp[4], uint16_t 
 
 	output.socketAddress.sin_addr.s_addr = output.localIp;
 	output.socketAddress.sin_family = DEFAULT_DOMAIN;
-	output.socketAddress.sin_port = DEFAULT_PORT;
+	output.socketAddress.sin_port = htons(DEFAULT_PORT);
 
 	output.socketLength = sizeof(output.socketAddress);
 
--- src/main.c
+++ src/main.c
@@ -82,6 +82,7 @@ void sender(uint8_t remote[4], uint8_t local[4], uint16_t port, string* filename
 
 void receiver(uint8_t remote[4], uint8_t local[4], uint16_t port, string* filename) {
 	socketParams_t socketParams = generateParams(local, remote, DEFAULT_PORT, Receiver);
+	socketParams.isMain = true;
 
 	createSocket(&socketParams);
 
