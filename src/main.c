2bd7dbe17ef8148e7381fc4cf858c55c719a4c54
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-19 10:45:29 +0100

    Ajout de la fonction arg_parser pour traiter les arguments de ligne de commande et initialiser RunInfo
    [WIP]

--- src/main.c
+++ src/main.c
@@ -13,6 +13,35 @@ struct RunInfo {
 };
 
 typedef struct RunInfo RunInfo_t;
+
+RunInfo_t arg_parser(string* argv, int arg_n) {
+	RunInfo_t runInfo;
+
+	if (arg_n == 1) {
+		runInfo.infoDefined = false;
+	} else if (arg_n == 6) {
+		string mode = argv[1]; // Either `-s` or `-r`
+		string local = argv[2]; // Local IPv4 as a string
+		string remote = argv[3]; // Remote IPv4 as a string
+		string port = argv[4]; // Port to use
+		string filename = argv[5]; // File to send
+
+		if (strcmp(mode, "-s") == 0) {
+			runInfo.type = Sender;
+		} else if (strcmp(mode, "-r") == 0) {
+			runInfo.type = Receiver;
+		} else {
+			fprintf(stderr, "Invalid mode\n");
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		fprintf(stderr, "Invalid number of arguments\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return runInfo;
+}
+
 int main(int argc, char** argv) {
 
 	RunInfo_t runInfo = arg_parser(argv, argc);
