cf65352693dc5fe42739ed1dd4ed98d38380ef7a
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-20 21:15:15 +0100

    Ajout de la bibliothèque ws2_32 lors de la compilation du projet avec GCC dans le script de construction

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 mkdir build
 
-gcc -Wall -s -O3 -m64 -std=c17 src/main.c src/definitions/*.c -o build/FileTransfer
+gcc -Wall -s -O3 -m64 -std=c17 src/main.c src/definitions/*.c -o build/FileTransfer -lws2_32
