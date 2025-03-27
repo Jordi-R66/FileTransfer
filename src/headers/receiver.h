b03aac38295445d5b462643def2f726f5cfc0af7
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-03-27 23:43:57 +0100

    Ajout de la documentation pour la fonction receiver, précisant les paramètres et le code d'état retourné.

--- src/headers/receiver.h
+++ src/headers/receiver.h
@@ -1,4 +1,12 @@
 #include "common.h"
 
+/**
+ * @brief Gets ready to receive and write a file from network
+ * 
+ * @param remote Remote IP address as an array of 4 uint8_t
+ * @param port Remote port
+ * @param filename Name to use when saving the file
+ * @return int8_t Error code, negative when failed to connect, positive when connected but something went wrong, and 0 if everything went right.
+ */
 int8_t receiver(uint8_t remote[4], uint16_t port, string* filename);
 
