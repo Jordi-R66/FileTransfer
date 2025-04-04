3c7f352d9d1f1c168a051a2fdefa107cabfb6653
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-04 19:34:17 +0200

    Implémentation de la fonction linuxRecv : ajout de la gestion des erreurs de connexion et de réception, ainsi que la vérification de la taille des données reçues. Mise à jour de l'en-tête linuxRecv.h pour inclure les définitions nécessaires.

--- src/definitions/unix/linuxRecv.c
+++ src/definitions/unix/linuxRecv.c
@@ -1,2 +1,68 @@
 #include "../../headers/unix/linuxRecv.h"
 
+int8_t linuxRecv(uint8_t remote[4], uint16_t port, string* filename) {
+	Endianness_t sysEndian = getEndian();
+	uint8_t buffer[BUFFER_SIZE];
+
+	Value16_t initShort = { COMM_INIT_VAL, 2, getEndian() };
+	Value64_t fileSize;
+
+	socketParams_t socketParams = generateParams(remote, port, Receiver);
+	socketParams.isMain = true;
+
+	sockAddrIn local_addr;
+	memset(&local_addr, 0, sizeof(local_addr));
+	local_addr.sin_family = DEFAULT_DOMAIN;
+	local_addr.sin_addr.s_addr = socketParams.Ip;
+	local_addr.sin_port = 0;
+
+	createUnixSocket(&socketParams);
+
+	int status = connect(socketParams.fd, (sockAddr*)&socketParams.socketAddress, socketParams.socketLength);
+
+	if (status < 0) {
+		close(socketParams.fd);
+
+		fprintf(stderr, "Couldn't connect to server at %u.%u.%u.%u:%hu\n", remote[0], remote[1], remote[2], remote[3], socketParams.port);
+		perror("");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Connected to server\n");
+
+	send(socketParams.fd, (char*)&initShort, sizeof(initShort), 0);
+
+	uint64_t totalRecv = 0;
+	int n_recv = recv(socketParams.fd, (char*)&fileSize, sizeof(Value64_t), 0);
+
+	if (fileSize.endian != sysEndian) {
+		swapEndianness(&fileSize.value, 64/8);
+	}
+
+	fprintf(stdout, "Ready to receive a %llu bytes.\n", fileSize.value);
+
+	FILE* fp = fopen(*filename, "w");
+
+	if (fp == NULL) {
+		fprintf(stderr, "Couldn't open file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	do {
+		n_recv = recv(socketParams.fd, (char*)buffer, BUFFER_SIZE, 0);
+		totalRecv += n_recv;
+
+		fwrite(buffer, 1, n_recv, fp);
+		memset(buffer, 0, BUFFER_SIZE);
+	} while (totalRecv < fileSize.value);
+
+	if (totalRecv > fileSize.value) {
+		fprintf(stderr, "Received more data than expected\n");
+		exit(EXIT_FAILURE);
+	}
+
+	close(socketParams.fd);
+	fclose(fp);
+
+	return 0;
+}
--- src/headers/unix/linuxRecv.h
+++ src/headers/unix/linuxRecv.h
@@ -1,3 +1,11 @@
-#include "../common.h"
+#pragma once
 
-int8_t linuxRecv(uint8_t remote[4], uint16_t port, string* filename);
+	#ifndef LINUX_RECV_H
+
+	#include "../common.h"
+	#include "linuxSocket.h"
+
+	int8_t linuxRecv(uint8_t remote[4], uint16_t port, string* filename);
+
+	#define LINUX_RECV_H
+#endif
