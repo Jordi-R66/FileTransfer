4db14e0220cd62771bfd4eb271be6f56ca04bbf6
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-04 21:09:52 +0200

    Mise à jour du script de construction pour inclure la bibliothèque ws2_32 lors de la compilation sous Windows et simplification de la fonction receiver en utilisant une variable de statut.

--- src/definitions/receiver.c
+++ src/definitions/receiver.c
@@ -1,76 +1,19 @@
 #include "../headers/receiver.h"
 
 #ifdef LINUX
-#include "../headers/unix/linuxRecv.h"
+	#include "../headers/unix/linuxRecv.h"
+#elif defined(WIN)
+	#include "../headers/win/windowsRecv.h"
 #endif
 
 int8_t receiver(uint8_t remote[4], uint16_t port, string* filename) {
-	/*Endianness_t sysEndian = getEndian();
-	uint8_t buffer[BUFFER_SIZE];
+	uint8_t status_code;
 
-	Value16_t initShort = { COMM_INIT_VAL, 2, getEndian() };
-	Value64_t fileSize;
+	#if defined(LINUX)
+		status_code = linuxRecv(remote, port, filename);
+	#elif defined(WIN)
+		status_code = winRecv(remote, port, filename);
+	#endif
 
-	socketParams_t socketParams = generateParams(remote, port, Receiver);
-	socketParams.isMain = true;
-
-	sockAddrIn local_addr;
-	memset(&local_addr, 0, sizeof(local_addr));
-	local_addr.sin_family = DEFAULT_DOMAIN;
-	local_addr.sin_addr.s_addr = socketParams.Ip;
-	local_addr.sin_port = 0;
-
-	createSocket(&socketParams);
-
-	int status = connect(socketParams.fd, (sockAddr*)&socketParams.socketAddress, socketParams.socketLength);
-
-	if (status < 0) {
-		closeSocket(&socketParams);
-		return -1;
-
-		fprintf(stderr, "Couldn't connect to server at %u.%u.%u.%u:%hu\n", remote[0], remote[1], remote[2], remote[3], socketParams.port);
-		perror("");
-		exit(EXIT_FAILURE);
-	}
-
-	printf("Connected to server\n");
-
-	send(socketParams.fd, (char*)&initShort, sizeof(initShort), 0);
-
-	uint64_t totalRecv = 0;
-	int n_recv = recv(socketParams.fd, (char*)&fileSize, sizeof(Value64_t), 0);
-
-	if (fileSize.endian != sysEndian) {
-		swapEndianness(&fileSize.value, 64/8);
-	}
-
-	fprintf(stdout, "Ready to receive a %llu bytes.\n", fileSize.value);
-
-	FILE* fp = fopen(*filename, "w");
-
-	if (fp == NULL) {
-		fprintf(stderr, "Couldn't open file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	do {
-		n_recv = recv(socketParams.fd, (char*)buffer, BUFFER_SIZE, 0);
-		totalRecv += n_recv;
-
-		fwrite(buffer, 1, n_recv, fp);
-		memset(buffer, 0, BUFFER_SIZE);
-	} while (totalRecv < fileSize.value);
-
-	if (totalRecv > fileSize.value) {
-		fprintf(stderr, "Received more data than expected\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fclose(fp);
-
-	closeSocket(&socketParams);
-
-	return 0;*/
-
-	return linuxRecv(remote, port, filename);
+	return status_code;
 }
