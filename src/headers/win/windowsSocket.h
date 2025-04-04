f80772560ba0efa1565ccb3bd899b7089d57b48d
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-04-04 21:09:45 +0200

    Réorganisation des inclusions et des directives de préprocesseur dans windowsSocket.h pour améliorer la structure du code.

--- src/headers/win/windowsSocket.h
+++ src/headers/win/windowsSocket.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#ifndef WINDOWS_SOCKETS_HEADER
-	#ifdef _WIN32
-		#define WINDOWS_SOCKETS_HEADER
+#include "../common.h"
 
-		#include "common.h"
-
+#ifdef WIN
+	#ifndef WINDOWS_SOCKETS_HEADER
 		void createWinSocket(socketParams_t* socketParams);
 		void closeWinSocket(socketParams_t* socketParams);
+
+		#define WINDOWS_SOCKETS_HEADER
 	#endif
 #endif
