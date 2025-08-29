a1ab2d3e93d0d7c84f443efbcaaf9db28de29f45
Jordi R <66806189+Jordi-R66@users.noreply.github.com>
2025-08-30 01:49:03 +0200

    sender now integrates winSend

--- src/definitions/sender.c
+++ src/definitions/sender.c
@@ -2,6 +2,8 @@
 
 #ifdef UNIX
 	#include "../headers/unix/linuxSend.h"
+#elif defined(WIN)
+	#include "../headers/win/winSend.h"
 #endif
 
 void sender(uint8_t local[4], uint16_t port, string* filename) {
