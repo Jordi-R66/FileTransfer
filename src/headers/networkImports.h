a8f6236898aabeb9e1aaa6bd1d0042f6d00ee26d
Jordi R <66806189+Jordi-R66@users.noreply.github.com>
2025-08-29 23:58:09 +0200

    Added the definition for some shutdown calls

--- src/headers/networkImports.h
+++ src/headers/networkImports.h
@@ -10,6 +10,10 @@
 	#include <winsock2.h>
 	#include <ws2tcpip.h>
 	#include <windows.h>
+
+	#define SHUT_RD SD_RECEIVE
+    #define SHUT_WR SD_SEND
+    #define SHUT_RDWR SD_BOTH
 #else
 	#error 'Unsupported OS'
 #endif
